fddcea7b4f686a095bb39437c1fcf07dbec21cce
SaifTTU <70453733+SaifTTU@users.noreply.github.com>
2021-10-01 16:18:56 -0500

    Update main.c

--- main.c
+++ main.c
@@ -6,32 +6,72 @@
 //  Copyright © 2021 Saif Chowdhury. All rights reserved.
 //
 
-#define boilerPlate int pid;MPI_Comm_rank(MPI_COMM_WORLD, &pid); int np; MPI_Comm_size(MPI_COMM_WORLD, &np);
+#define boilerPlate MPI_Status status; MPI_Init(&argc, &argv); int pid;MPI_Comm_rank(MPI_COMM_WORLD, &pid); int np; MPI_Comm_size(MPI_COMM_WORLD, &np); int message[] = {0};
+#define send(X) MPI_Send(message, 2, MPI_INT, X, 0, MPI_COMM_WORLD); printf("Process %d sent message to %d", pid, X);
+#define recieve(X) MPI_Recv(message, 2, MPI_INT, X, 0, MPI_COMM_WORLD, &status); printf("Process %d recieved message from %d", pid, X);
 
-#define send(X) MPI_Send(message, 10, MPI_INT, X, 0, MPI_COMM_WORLD);
-#define recieve(Y) MPI_Recv(Y, 1, MPI_INT, Y, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-
-#define message(X) int[] message = { X , X , X, X}
 #define msg message[0]
-#define msg(X) message[X]
 
-#define rank(X) pid==X
+#define pid(X) pid==X
+
+#define print(X) printf(X);
+
+#define finalize MPI_Finalize();
+
+#define for(X) for(int i=0;i<X;i++)
+
+#define round(X) round = X;
+#define round(X, Y, Z) round = X; if(pid==Z){MPI_Send(&round, 2, MPI_INT, Y, 0, MPI_COMM_WORLD); } if(pid==Y){MPI_Recv(&round, 2, MPI_INT, Z, 0, MPI_COMM_WORLD, &status); }
+
+#define round_(X) round==X
+
+#define send_and_recieve(X,Y) if(pid==Y){MPI_Send(message, 2, MPI_INT, X, 0, MPI_COMM_WORLD); printf("Process %d sent message to %d\n", pid, X);} if(pid==X){MPI_Recv(message, 2, MPI_INT, Y, 0, MPI_COMM_WORLD, &status); printf("Process %d recieved message from %d\n", pid, Y);}
+
+
 
 #include <stdio.h>
 #include <mpi.h>
 
-int main(int argc, const char * argv[]) {
-    
+int main(int argc, char** argv) {
+
     boilerPlate
-    message(0)
-    
-    if(rank(0)){
-        send(1)
+
+        int round = 0;
+
+    if (round_(0)) {
+        send_and_recieve(3, 7);
+        round(1, 3, 7)
+        send_and_recieve(2, 6);
+        round(1, 2, 6)
+        send_and_recieve(1, 5);
+        round(1, 1, 5)
+        send_and_recieve(0, 4);
+        round(1, 0, 4)
+
     }
-    if(rank(1)){
-        recieve(0)
+
+    if (round_(1)) {
+        send_and_recieve(2, 4);
+        round(2, 2, 4)
+        send_and_recieve(1, 3);
+        round(2, 1, 3)
+            //round(2)
     }
-    
-    
-    return 0;
+
+    if (round_(2)) {
+        send_and_recieve(0, 2);
+        round(3, 0, 2)
+        send_and_recieve(0, 1);
+        round(3, 0, 1)
+    }
+
+
+
+
+
+
+
+    finalize
+
+        return 0;
 }
